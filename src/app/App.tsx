cd6ff1983f85bcbdb6fe7d12d4f26e8e87674b48
Gretlin <129331213+gretlinp@users.noreply.github.com>
2026-04-11 14:26:26 +0300

    routing engine setup v1 - map shows, from a to b route shows

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import { AppHeaderActions } from '@app/navigation/AppHeaderActions'
 import { AppLayout } from '@components/Layout'
-import { AppMap } from '@components/Map'
 import { Outlet } from '@lib/router'
 
 function App() {
   return (
     <AppLayout headerActions={<AppHeaderActions />}>
       <Outlet />
-      <AppMap />
     </AppLayout>
   )
 }
